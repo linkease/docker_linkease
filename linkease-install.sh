cf3f00ab19866b2d8031c92c0b49fce5ee248f1a
janson <janson@gmail.com>
2022-08-20 17:46:02 +0800

    support linkease media

--- linkease-install.sh
+++ linkease-install.sh
@@ -4,6 +4,7 @@ PLATFORM=$1
 if [ -z "$PLATFORM" ]; then
     LINKEASE_FILE="linkease.x86_64"
     HEIC_FILE="heif-converter.x86_64"
+    MEDIA_FILE="linkease-media.x86_64"
 else
     case "$PLATFORM" in
         linux/386)
@@ -12,16 +13,20 @@ else
         linux/amd64)
             LINKEASE_FILE="linkease.x86_64"
             HEIC_FILE="heif-converter.x86_64"
+            MEDIA_FILE="linkease-media.x86_64"
             ;;
         linux/arm/v6)
             LINKEASE_FILE="linkease.arm"
+            MEDIA_FILE="linkease-media.arm"
             ;;
         linux/arm/v7)
             LINKEASE_FILE="linkease.arm"
+            MEDIA_FILE="linkease-media.arm"
             ;;
         linux/arm64|linux/arm64/v8)
             LINKEASE_FILE="linkease.aarch64"
             HEIC_FILE="heif-converter.aarch64"
+            MEDIA_FILE="linkease-media.aarch64"
             ;;
         linux/ppc64le)
             LINKEASE_FILE=""
@@ -37,10 +42,12 @@ fi
 [ -z "${LINKEASE_FILE}" ] && echo "Error: Not supported OS Architecture" && exit 1
 
 cp /dest/linkease-binary/${LINKEASE_FILE} /usr/bin/linkease
+cp /dest/linkease-binary/${MEDIA_FILE} /usr/bin/linkease-media
 if [ ! -z "${HEIC_FILE}" ]; then
   cp /dest/linkease-binary/${HEIC_FILE} /usr/bin/heif-converter
   chmod +x /usr/bin/heif-converter
 fi
 
 chmod +x /usr/bin/linkease
+chmod +x /usr/bin/linkease-media
 
--- prepare-dl.sh
+++ prepare-dl.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+set -ex
+
 VER=0.9.0
+MEDIA=5.0.1
 PWD=`pwd`
 rm -rf dest.tar.gz dest
 mkdir -p dest
-wget -O dest.tar.gz http://fw.koolcenter.com/binary/LinkEase/LinuxStorage/linkease-binary-${VER}.tar.gz > /dev/null 2>&1
+wget -O dest.tar.gz http://fw.koolcenter.com/binary/LinkEase/LinuxStorage/linkease-binary-${VER}.tar.gz
 tar -zxf ./dest.tar.gz -C ./dest/
 mv ./dest/linkease-binary-${VER} ./dest/linkease-binary
 cp ./linkease-install.sh ./dest/
+rm -f ./dest.tar.gz
+
+wget -O media.tar.gz http://fw.koolcenter.com/binary/LinkEase/LinuxStorage/linkease-media-binary-${MEDIA}.tar.gz
+tar -zxf ./media.tar.gz -C ./dest/
+cp ./dest/linkease-media-binary-${MEDIA}/* ./dest/linkease-binary/
+rm -rf ./media.tar.gz ./dest/linkease-media-binary-${MEDIA}
+
+
