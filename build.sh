e6dbb80f440d838544fffc2496cde48afe031e4a
Janson <gandancing@gmail.com>
2022-12-13 23:40:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 #docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t linkease/linkease:0.3.60 -f ./Dockerfile.architecture --push .
-docker buildx build --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t linkease/linkease:latest -f ./Dockerfile.architecture --push .
+
+docker buildx build \
+        --build-arg "HTTP_PROXY=http://127.0.0.1:11282/" \
+        --build-arg "HTTPS_PROXY=http://127.0.0.1:11282/" \
+        --platform linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 -t linkease/linkease:latest -f ./Dockerfile.architecture --push .
+
+
+
